c160852bdcc35c7299e57bfc5b6781d7ea9ffb52
Hetvi512 <140398077+Hetvi512@users.noreply.github.com>
2023-09-22 18:08:12 +0530

    Assignment 3 Tops directory

--- Assignment 3 Tops/odd even using ternary.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int a;
-	printf("Enter number:");
-	scanf("%d",&a);
-	(a%2==0)?printf("The number is even"):printf("The number is odd");
-	return 0;
-}
--- Assignment 3 Tops/print name,age,birthdate,address.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	printf("My name is Hetvi\n");
-	printf("My birth date is 5/12/2001\n");
-	printf("My age is 21\n");
-	printf("My address is \n B/103,Shree Stavan\n Ghodasar\n Ahmedabad");
-	return 0;
-}
